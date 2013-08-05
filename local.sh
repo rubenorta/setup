b73fac5a824680613e8d9c7977e07d66f892cfbf
Ruben Orta <ruben.orta@gmail.com>
2013-08-05 17:37:18 +0200

    Adding commands to local.sh

--- local.sh
+++ local.sh
@@ -8,3 +8,12 @@ git config --global user.email "ruben.orta@gmail.com"
 cd $HOME
 git clone http://github.com/rubenorta/bitstarter
 cd bitstarter
+heroku login
+heroky keys:add
+heroku apps:destroy rubenorta-bitstarter-s-mooc
+heroku apss:destroy rubenorta-bitstarter-mooc
+heroku apps:create rubenorta-bitstarter-s-mooc --remote staging-heroku
+heroku apps:create rubenorta-bitstarter-mooc --remote production-heroku
+
+# git push staging-heroku staging:master
+# git push production-heroku master:master
